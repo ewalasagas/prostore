1948bdf30626b441542f8e9586542196f4b0f8b2
Elaine Alasagas <ealasagas@gmail.com>
2025-04-11 03:55:36 -0700

    sample data tsx file

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,10 @@
+import ProductList from "@/components/shared/header/product/product-list";
+import sampleData from "@/sample-data";
+
 const Homepage = () => {
-    return <>prostore</>;
-}
+    return <>
+        <ProductList data={sampleData.products} title="Newest Arrivals" limit={4}/>
+    </>;
+};
  
 export default Homepage;
