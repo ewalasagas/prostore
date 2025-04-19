c1afa5f76cf841caf892ff88278294ed83545335
Elaine Alasagas <ealasagas@gmail.com>
2025-04-19 08:53:07 -0700

    fixed prisma client connections

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,7 +1,7 @@
 'use server';
-import { prisma } from "@/db/prisma";
 import { convertToPlainObject } from "../utils";
 import { LATEST_PRODUCTS_LIMIT } from "../constants";
+import { prisma } from "@/db/prisma";
 
 // Get latest products
 export async function getLatestProducts() {
@@ -11,4 +11,11 @@ export async function getLatestProducts() {
     });
 
     return convertToPlainObject(data);
+}
+
+//Get single product by it's slug
+export async function getProductBySlug(slug: string) {
+    return await prisma.product.findFirst({
+        where: {slug: slug},
+    });
 }
